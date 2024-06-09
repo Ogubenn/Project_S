b701c0018046c2284dc5f4ce54215c3927da8e0c
Oğulcan Durkan <ogulcan285@outlook.com>
2024-06-10 01:19:33 +0300

    BatteryGlow

---
+++ Assets/Scrips/BatterySpawn.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatterySpawn : MonoBehaviour
+{
+    [SerializeField] GameObject gameObject;
+    public List<Transform> TransformList;
+
+
+}//class
